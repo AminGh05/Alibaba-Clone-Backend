a5011139a988954951d7d776c0cbf053082e3a02
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2025-03-24 23:16:51 +0330

    added repository pattern generic interface

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace AlibabaClone.Domain.Framework.Interfaces.Repositories
+{
+	public interface IRepository<TEntity, TPrimarayKey> where TEntity : class
+	{
+		Task InsertAsync(TEntity entity);
+		Task<List<TEntity>> GetAllAsync();
+		Task<TEntity> GetByIdAsync(TPrimarayKey id);
+		void UpdateAsync(TEntity entity);
+		Task DeleteAsync(TEntity entity);
+		Task DeleteAsync(TPrimarayKey id);
+	}
+}
