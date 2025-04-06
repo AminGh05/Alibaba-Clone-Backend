6efb8dbff3e2fc0b33a04b5b2c24347dc7b3346a
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2025-04-06 20:45:13 +0330

    feat(mapper): added auto-mapper profiles for city and transportation

---
+++ AlibabaClone.Application/Common/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AlibabaClone.Application.DTOs.CityDTOs;
+using AlibabaClone.Application.DTOs.TransportationDTOs;
+using AlibabaClone.Domain.Aggregates.LocationAggregates;
+using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using AutoMapper;
+
+namespace AlibabaClone.Application.Common.Profiles
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Transportation, TransportationSearchResultDto>()
+				.ForMember(dest => dest.CompanyTitle, opt => opt.MapFrom(src => src.Company.Title))
+				.ForMember(dest => dest.FromLocationTitle, opt => opt.MapFrom(src => src.FromLocation.Title))
+				.ForMember(dest => dest.ToLocationTitle, opt => opt.MapFrom(src => src.ToLocation.Title))
+				.ForMember(dest => dest.FromCityTitle, opt => opt.MapFrom(src => src.FromLocation.City.Title))
+				.ForMember(dest => dest.ToCityTitle, opt => opt.MapFrom(src => src.ToLocation.City.Title));
+
+			CreateMap<City, CityDto>();
+		}
+	}
+}
