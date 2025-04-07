460c55145a3ee6ccc9e13f207b24edc570b890f3
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2025-04-07 17:29:18 +0330

    feat(controller): added controllers for city and transportation

---
+++ AlibabaClone.WebAPI/Controllers/CityController.cs
@@ -0,0 +1,37 @@
+﻿using AlibabaClone.Application.Interfaces;
+using AlibabaClone.Application.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlibabaClone.WebAPI.Controllers
+{
+	public class CityController : ControllerBase
+	{
+		private readonly ICityService _cityService;
+
+		public CityController(ICityService cityService)
+		{
+			_cityService = cityService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllCities()
+		{
+			var result = await _cityService.GetCitiesAsync();
+			if (result.IsSuccess)
+			{
+				return Ok(result);
+			}
+
+			// any unsuccessful status
+			switch (result.Status)
+			{
+				case ResultStatus.NotFound:
+					return NotFound(result.ErrorMessage);
+				case ResultStatus.ValidationError:
+					return BadRequest(result.ErrorMessage);
+				default:
+					return StatusCode(500, result.ErrorMessage);
+			}
+		}
+	}
+}
---
+++ AlibabaClone.WebAPI/Controllers/TransportationController.cs
@@ -0,0 +1,45 @@
+﻿using AlibabaClone.Application.DTOs.TransportationDTOs;
+using AlibabaClone.Application.Interfaces;
+using AlibabaClone.Application.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlibabaClone.WebAPI.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TransportationController : ControllerBase
+	{
+		private readonly ITransportationService _transportationService;
+
+		public TransportationController(ITransportationService transportationService)
+		{
+			_transportationService = transportationService;
+		}
+
+		[HttpGet("search")]
+		public async Task<IActionResult> SearchTransportations([FromBody] TransportationSearchRequestDto searchRequest)
+		{
+			if (searchRequest == null)
+			{
+				return BadRequest("Invalid search request");
+			}
+
+			var result = await _transportationService.SearchTransportationsAsync(searchRequest);
+			if (result.IsSuccess)
+			{
+				return Ok(result);
+			}
+
+			// any unsuccessful status
+			switch (result.Status)
+			{
+				case ResultStatus.NotFound: 
+					return NotFound(result.ErrorMessage);
+				case ResultStatus.ValidationError: 
+					return BadRequest(result.ErrorMessage);
+				default:
+					return StatusCode(500, result.ErrorMessage);
+			}
+		}
+	}
+}
