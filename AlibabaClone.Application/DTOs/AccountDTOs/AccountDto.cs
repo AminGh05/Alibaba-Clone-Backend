ecefc2b1b6abb5f946d3fdcf841dcc3b3d7e461c
AminGh05 <mo.ghoorchian@gmail.com>
2025-06-05 00:04:49 +0330

    feat(dto): added DTOs for account, authentication-reponse, login and register

---
+++ AlibabaClone.Application/DTOs/AccountDTOs/AccountDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AlibabaClone.Application.DTOs.AccountDTOs
+{
+    public class AccountDto
+    {
+        public int Id { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { get; set; }
+        public string? Email { get; set; }
+        public long? PersonId { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
---
+++ AlibabaClone.Application/DTOs/AccountDTOs/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AlibabaClone.Application.DTOs.AccountDTOs
+{
+    public class LoginRequestDto
+    {
+        public string PhoneNumber { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
