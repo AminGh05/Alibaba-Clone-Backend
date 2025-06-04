5041866aeeec340f7aed3dd13d550afa6cbb64a4
AminGh05 <mo.ghoorchian@gmail.com>
2025-06-05 00:19:54 +0330

    feat(hash): added a hash function and a generator

---
+++ AlibabaClone.Application/Common/Utils/PasswordHasher.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+
+namespace AlibabaClone.Application.Common.Utils
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(16);
+            // create the 32 hash
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+            // an array of the salt and the hash
+            byte[] hashBytes = new byte[48];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 32);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPass)
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashedPass);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            // hash the input to check with the hashed one
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+            // check password and its hash
+            for (int i = 0; i < 32; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
