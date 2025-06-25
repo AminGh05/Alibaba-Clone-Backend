f1478a5a1bc988eba07914b3ba21ee38809ed09e
AminGh05 <mo.ghoorchian@gmail.com>
2025-06-25 19:56:57 +0330

    feat & reformat: added ticket-order and transaction-type, modified ticket and transaction

--- AlibabaClone.Domain/Aggregates/TransactionAggregates/Ticket.cs
+++ AlibabaClone.Domain/Aggregates/TransactionAggregates/Ticket.cs
@@ -6,21 +6,20 @@ namespace AlibabaClone.Domain.Aggregates.TransactionAggregates
 {
     public class Ticket : Entity<long>
     {
-		public long TransportationId { get; set; }
+		public long TicketOrderId { get; set; }
 		public long SeatId { get; set; }
 		public long TravelerId { get; set; }
-		public long BuyerId { get; set; }
 		public DateTime CreatedAt { get; set; }
-		public long? CompanionId { get; set; }
+        public DateTime? CanceledAt { get; set; }
+        public long? CompanionId { get; set; }
 		public short TicketStatusId { get; set; }
 		public required string SerialNumber { get; set; }
 		public string? Description { get; set; }
 
 		#region Navigation Properties
-		public virtual Transportation Transportation { get; set; }
+		public virtual TicketOrder TicketOrder { get; set; }
 		public virtual Seat Seat { get; set; }
 		public virtual Person Traveler { get; set; }
-		public virtual Account Buyer { get; set; }
 		public virtual Person? Companion { get; set; }
 		public virtual TicketStatus TicketStatus { get; set; }
 		#endregion
---
+++ AlibabaClone.Domain/Aggregates/TransactionAggregates/TicketOrder.cs
@@ -0,0 +1,22 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using AlibabaClone.Domain.Framework.Base;
+
+namespace AlibabaClone.Domain.Aggregates.TransactionAggregates
+{
+    public class TicketOrder : Entity<long>
+    {
+        public long TransportationId { get; set; }
+        public long BuyerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string SerialNumber { get; set; }
+        public string? Description { get; set; }
+
+        #region Navigation Properties
+        public virtual ICollection<Ticket> Tickets { get; set; } = [];
+        public virtual Transportation Transportation { get; set; }
+        public virtual Account Buyer { get; set; }
+        public virtual Transaction Transaction { get; set; }
+        #endregion
+    }
+}
