5babfc92cc18201145310cda58d24fe648066bb5
AminGh05 <mo.ghoorchian@gmail.com>
2025-06-28 23:19:48 +0330

    feat(dto): added some dtos to edit and upsert into profile

---
+++ AlibabaClone.Application/DTOs/AccountDTOs/EditPasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlibabaClone.Application.DTOs.AccountDTOs
+{
+    public class EditPasswordDto
+    {
+        [Required(ErrorMessage = "Old password is required")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required")]
+        [MinLength(8, ErrorMessage = "At least 8 chars")]
+        public string NewPassword { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password doesn't match")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
---
+++ AlibabaClone.Application/DTOs/AccountDTOs/UpsertPersonDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlibabaClone.Application.DTOs.AccountDTOs
+{
+    public class UpsertPersonDto
+    {
+        public long Id { get; set; }
+        public long CreatorId { get; set; }
+
+        [Required(ErrorMessage = "Firstname is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "National Id number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "National ID number must be exactly 10 digits")]
+        public string IdNumber { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public short GenderId { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Birth date is required")]
+        public DateTime BirthDate { get; set; }
+    }
+}
