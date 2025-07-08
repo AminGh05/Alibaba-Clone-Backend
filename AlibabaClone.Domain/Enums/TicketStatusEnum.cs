51cfd120caad3148026d53c1c815aeb6eb32017e
AminGh05 <mo.ghoorchian@gmail.com>
2025-07-09 01:06:46 +0330

    added required enums

---
+++ AlibabaClone.Domain/Enums/TicketStatusEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace AlibabaClone.Domain.Enums
+{
+    public enum TicketStatusEnum
+    {
+        Reserved = 1,
+        Paid = 2,
+        CancelledByUser = 3,
+        CancelledBySystem = 4,
+        Used = 5,
+        Expired = 6
+    }
+}
---
+++ AlibabaClone.Domain/Enums/VehicleTypeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace AlibabaClone.Domain.Enums
+{
+    public enum VehicleTypeEnum
+    {
+        Airplane = 1,
+        Train = 2,
+        Bus = 3
+    }
+}
