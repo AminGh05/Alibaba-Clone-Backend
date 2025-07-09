681af3b14a0bd78ed28a90d2187ed7a23e85956a
AminGh05 <mo.ghoorchian@gmail.com>
2025-07-09 20:06:54 +0330

    feat(seats): added endpoint and flow of getting seats of a transportation

--- AlibabaClone.Application/Interfaces/ITransportationService.cs
+++ AlibabaClone.Application/Interfaces/ITransportationService.cs
@@ -6,5 +6,6 @@ namespace AlibabaClone.Application.Interfaces
 	public interface ITransportationService
 	{
 		Task<Result<IEnumerable<TransportationSearchResultDto>>> SearchTransportationsAsync(TransportationSearchRequestDto requestDto);
+		Task<Result<List<TransportationSeatDto>>> GetTransportationSeatsAsync(long transportationId);
 	}
 }
--- AlibabaClone.Application/Services/TransportationService.cs
+++ AlibabaClone.Application/Services/TransportationService.cs
@@ -4,23 +4,46 @@ using AlibabaClone.Application.Result;
 using AlibabaClone.Domain.Framework.Interfaces;
 using AlibabaClone.Domain.Framework.Interfaces.Repositories.TransportationRepositories;
 using AutoMapper;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AlibabaClone.Application.Services
 {
-	public class TransportationService : ITransportationService
+    public class TransportationService : ITransportationService
 	{
 		private readonly ITransportationRepository _transportationRepository;
+		private readonly ISeatRepository _seatRepository;
 		private readonly IMapper _mapper;
 		private readonly IUnitOfWork _unitOfWork;
 
-		public TransportationService(ITransportationRepository transportationRepository, IMapper mapper, IUnitOfWork unitOfWork)
+		public TransportationService(ITransportationRepository transportationRepository, 
+			ISeatRepository seatRepository,
+			IMapper mapper, 
+			IUnitOfWork unitOfWork)
 		{
 			_transportationRepository = transportationRepository;
+			_seatRepository = seatRepository;
 			_mapper = mapper;
 			_unitOfWork = unitOfWork;
 		}
 
-		public async Task<Result<IEnumerable<TransportationSearchResultDto>>> SearchTransportationsAsync(TransportationSearchRequestDto requestDto)
+        public async Task<Result<List<TransportationSeatDto>>> GetTransportationSeatsAsync(long transportationId)
+        {
+            var transportation = await _transportationRepository.GetByIdAsync(transportationId);
+			if (transportation == null)
+			{
+				return Result<List<TransportationSeatDto>>.Error(null, "Transportation not found"); 
+			}
+
+			var seats = await _seatRepository.GetSeatsByVehicleIdAsync(transportation.VehicleId);
+			if (seats == null || seats.Count != 0)
+			{
+				return Result<List<TransportationSeatDto>>.Success(_mapper.Map<List<TransportationSeatDto>>(seats));
+			}
+
+			return Result<List<TransportationSeatDto>>.NotFound(null);
+        }
+
+        public async Task<Result<IEnumerable<TransportationSearchResultDto>>> SearchTransportationsAsync(TransportationSearchRequestDto requestDto)
 		{
 			var result = await _transportationRepository.SearchTransportationsAsync(
 				vehicleTypeId: requestDto.VehicleTypeId,
