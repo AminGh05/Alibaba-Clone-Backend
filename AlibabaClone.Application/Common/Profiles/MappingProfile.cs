27a0ac0406c63ea71e0ccf41edea919c672c52b3
AminGh05 <mo.ghoorchian@gmail.com>
2025-07-09 19:52:22 +0330

    feat(dto): added TransportationSeatDto and its mapping to Seat

--- AlibabaClone.Application/Common/Profiles/MappingProfile.cs
+++ AlibabaClone.Application/Common/Profiles/MappingProfile.cs
@@ -6,70 +6,76 @@ using AlibabaClone.Domain.Aggregates.AccountAggregates;
 using AlibabaClone.Domain.Aggregates.LocationAggregates;
 using AlibabaClone.Domain.Aggregates.TransactionAggregates;
 using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using AlibabaClone.Domain.Enums;
 using AutoMapper;
 
 namespace AlibabaClone.Application.Common.Profiles
 {
-	public class MappingProfile : Profile
-	{
-		public MappingProfile()
-		{
-			CreateMap<Transportation, TransportationSearchResultDto>()
-				.ForMember(dest => dest.CompanyTitle, opt => opt.MapFrom(src => src.Company.Title))
-				.ForMember(dest => dest.FromLocationTitle, opt => opt.MapFrom(src => src.FromLocation.Title))
-				.ForMember(dest => dest.ToLocationTitle, opt => opt.MapFrom(src => src.ToLocation.Title))
-				.ForMember(dest => dest.FromCityTitle, opt => opt.MapFrom(src => src.FromLocation.City.Title))
-				.ForMember(dest => dest.ToCityTitle, opt => opt.MapFrom(src => src.ToLocation.City.Title))
-				.ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.BasePrice));
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Transportation, TransportationSearchResultDto>()
+                .ForMember(dest => dest.CompanyTitle, opt => opt.MapFrom(src => src.Company.Title))
+                .ForMember(dest => dest.FromLocationTitle, opt => opt.MapFrom(src => src.FromLocation.Title))
+                .ForMember(dest => dest.ToLocationTitle, opt => opt.MapFrom(src => src.ToLocation.Title))
+                .ForMember(dest => dest.FromCityTitle, opt => opt.MapFrom(src => src.FromLocation.City.Title))
+                .ForMember(dest => dest.ToCityTitle, opt => opt.MapFrom(src => src.ToLocation.City.Title))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.BasePrice));
 
-			CreateMap<City, CityDto>();
+            CreateMap<City, CityDto>();
 
-			CreateMap<Account, AccountDto>()
-				.ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.AccountRoles.Select(x => x.Role.Title)));
+            CreateMap<Account, AccountDto>()
+                .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.AccountRoles.Select(x => x.Role.Title)));
 
-			CreateMap<AccountDto, Account>()
-				.ForMember(dest => dest.AccountRoles, opt => opt.Ignore());
+            CreateMap<AccountDto, Account>()
+                .ForMember(dest => dest.AccountRoles, opt => opt.Ignore());
 
-			CreateMap<Account, ProfileDto>()
-				.ForMember(dest => dest.AccountPhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-				.ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-				.ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.Balance))
-				.ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Person != null ? src.Person.FirstName : ""))
-				.ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.Person != null ? src.Person.LastName : ""))
-				.ForMember(dest => dest.IdNumber, opt => opt.MapFrom(src => src.Person != null ? src.Person.IdNumber : ""))
-				.ForMember(dest => dest.PersonPhoneNumber, opt => opt.MapFrom(src => src.Person != null ? src.Person.PhoneNumber : ""))
-				.ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.Person != null ? src.Person.BirthDate : (DateTime?) null))
-				.ForMember(dest => dest.IBAN, opt => opt.MapFrom(src => src.BankAccount != null ? src.BankAccount.IBAN : ""))
-				.ForMember(dest => dest.BankAccountNumber, opt => opt.MapFrom(src => src.BankAccount != null ? src.BankAccount.BankAccountNumber : ""))
-				.ForMember(dest => dest.CardNumber, opt => opt.MapFrom(src => src.BankAccount != null ? src.BankAccount.CardNumber : ""));
+            CreateMap<Account, ProfileDto>()
+                .ForMember(dest => dest.AccountPhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.Balance))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Person != null ? src.Person.FirstName : ""))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.Person != null ? src.Person.LastName : ""))
+                .ForMember(dest => dest.IdNumber, opt => opt.MapFrom(src => src.Person != null ? src.Person.IdNumber : ""))
+                .ForMember(dest => dest.PersonPhoneNumber, opt => opt.MapFrom(src => src.Person != null ? src.Person.PhoneNumber : ""))
+                .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.Person != null ? src.Person.BirthDate : (DateTime?)null))
+                .ForMember(dest => dest.IBAN, opt => opt.MapFrom(src => src.BankAccount != null ? src.BankAccount.IBAN : ""))
+                .ForMember(dest => dest.BankAccountNumber, opt => opt.MapFrom(src => src.BankAccount != null ? src.BankAccount.BankAccountNumber : ""))
+                .ForMember(dest => dest.CardNumber, opt => opt.MapFrom(src => src.BankAccount != null ? src.BankAccount.CardNumber : ""));
 
             CreateMap<Person, PersonDto>()
-				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.CreatorId, opt => opt.MapFrom(src => src.CreatorId))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
                 .ForMember(dest => dest.IdNumber, opt => opt.MapFrom(src => src.IdNumber))
                 .ForMember(dest => dest.GenderId, opt => opt.MapFrom(src => src.GenderId))
-				.ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
                 .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate))
                 .ReverseMap();
 
-			CreateMap<TicketOrder, TicketOrderSummaryDto>()
-				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-				.ForMember(dest => dest.SerialNumber, opt => opt.MapFrom(src => src.SerialNumber))
-				.ForMember(dest => dest.BoughtAt, opt => opt.MapFrom(src => src.CreatedAt))
-				.ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Transaction != null ? src.Transaction.FinalAmount : 0))
-				.ForMember(dest => dest.TravelStartDate, opt => opt.MapFrom(src => src.Transportation.StartDateTime))
-				.ForMember(dest => dest.TravelEndDate, opt => opt.MapFrom(src => src.Transportation.EndDateTime))
-				.ForMember(dest => dest.FromCity, opt => opt.MapFrom(src => src.Transportation.FromLocation.City.Title))
-				.ForMember(dest => dest.ToCity, opt => opt.MapFrom(src => src.Transportation.ToLocation.City.Title))
-				.ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => src.Transportation.Company.Title))
-				.ForMember(dest => dest.VehicleTypeId, opt => opt.MapFrom(src => src.Transportation.Vehicle.VehicleTypeId))
-				.ForMember(dest => dest.VehicleName, opt => opt.MapFrom(src => src.Transportation.Vehicle.Title));
+            CreateMap<TicketOrder, TicketOrderSummaryDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.SerialNumber, opt => opt.MapFrom(src => src.SerialNumber))
+                .ForMember(dest => dest.BoughtAt, opt => opt.MapFrom(src => src.CreatedAt))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Transaction != null ? src.Transaction.FinalAmount : 0))
+                .ForMember(dest => dest.TravelStartDate, opt => opt.MapFrom(src => src.Transportation.StartDateTime))
+                .ForMember(dest => dest.TravelEndDate, opt => opt.MapFrom(src => src.Transportation.EndDateTime))
+                .ForMember(dest => dest.FromCity, opt => opt.MapFrom(src => src.Transportation.FromLocation.City.Title))
+                .ForMember(dest => dest.ToCity, opt => opt.MapFrom(src => src.Transportation.ToLocation.City.Title))
+                .ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => src.Transportation.Company.Title))
+                .ForMember(dest => dest.VehicleTypeId, opt => opt.MapFrom(src => src.Transportation.Vehicle.VehicleTypeId))
+                .ForMember(dest => dest.VehicleName, opt => opt.MapFrom(src => src.Transportation.Vehicle.Title));
 
-			CreateMap<Transaction, TransactionDto>()
-				.ForMember(dest => dest.TransactionType, opt => opt.MapFrom(src => src.TransactionType.Title));
-			CreateMap<TransactionDto, Transaction>();
+            CreateMap<Transaction, TransactionDto>()
+                .ForMember(dest => dest.TransactionType, opt => opt.MapFrom(src => src.TransactionType.Title));
+            CreateMap<TransactionDto, Transaction>();
+
+            CreateMap<Seat, TransportationSeatDto>()
+                .ForMember(dest => dest.IsReserved, opt => opt.MapFrom(src => src.Tickets.Any(t => t.TicketStatusId == (int)TicketStatusEnum.Reserved)))
+                .ForMember(dest => dest.GenderId, opt => opt.MapFrom(src => src.Tickets.Any(t => t.TicketStatusId == (int)TicketStatusEnum.Reserved) ?
+                src.Tickets.First(t => t.TicketStatusId == (int)TicketStatusEnum.Reserved).Traveler.GenderId : (short?)null)); 
         }
-	}
+    }
 }
