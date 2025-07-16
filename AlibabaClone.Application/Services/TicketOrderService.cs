947363c12d2f5038c3dc232db83fd38d93c9ac35
AminGh05 <mo.ghoorchian@gmail.com>
2025-07-16 20:19:44 +0330

    reformat(service): modifed TicketOrderService to sync with frontend

--- AlibabaClone.Application/Services/TicketOrderService.cs
+++ AlibabaClone.Application/Services/TicketOrderService.cs
@@ -1,5 +1,4 @@
-﻿using AlibabaClone.Application.DTOs.AccountDTOs;
-using AlibabaClone.Application.DTOs.TransportationDTOs;
+﻿using AlibabaClone.Application.DTOs.TransportationDTOs;
 using AlibabaClone.Application.Interfaces;
 using AlibabaClone.Application.Result;
 using AlibabaClone.Domain.Aggregates.TransactionAggregates;
@@ -18,11 +17,9 @@ namespace AlibabaClone.Application.Services
         private readonly IAccountRepository _accountRepository;
         private readonly ITransportationRepository _transportationRepository;
         private readonly ISeatRepository _seatRepository;
-        private readonly IPersonRepository _personRepository;
         private readonly ITicketOrderRepository _ticketOrderRepository;
         private readonly ITicketRepository _ticketRepository;
         private readonly ITransportationLockService _transportationLockService;
-        private readonly IPersonService _personService;
         private readonly IAccountService _accountService;
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
@@ -30,11 +27,9 @@ namespace AlibabaClone.Application.Services
         public TicketOrderService(IAccountRepository accountRepository,
             ITransportationRepository transportationRepository,
             ISeatRepository seatRepository,
-            IPersonRepository personRepository,
             ITicketOrderRepository ticketOrderRepository,
             ITicketRepository ticketRepository,
             ITransportationLockService transportationLockService,
-            IPersonService personService,
             IAccountService accountService,
             IMapper mapper,
             IUnitOfWork unitOfWork)
@@ -42,11 +37,9 @@ namespace AlibabaClone.Application.Services
             _accountRepository = accountRepository;
             _transportationRepository = transportationRepository;
             _seatRepository = seatRepository;
-            _personRepository = personRepository;
             _ticketOrderRepository = ticketOrderRepository;
             _ticketRepository = ticketRepository;
             _transportationLockService = transportationLockService;
-            _personService = personService;
             _accountService = accountService;
             _mapper = mapper;
             _unitOfWork = unitOfWork;
@@ -84,8 +77,7 @@ namespace AlibabaClone.Application.Services
                 }
 
                 var finalAmount = baseAmount;
-                await AssignSeatsIfDynamic(transportation.VehicleId, dto.Travellers);
-                await UpsertTravellers(accountId, dto.Travellers);
+                await AssignSeats(transportation.VehicleId, dto.Travellers);
 
                 // add the ticket order by the info we have
                 TicketOrder ticketOrder = new()
@@ -97,7 +89,7 @@ namespace AlibabaClone.Application.Services
                     TransportationId = dto.TransportationId
                 };
                 await _ticketOrderRepository.InsertAsync(ticketOrder);
-                
+
                 foreach (var traveller in dto.Travellers)
                 {
                     if (!traveller.SeatId.HasValue)
@@ -133,46 +125,16 @@ namespace AlibabaClone.Application.Services
             return "";
         }
 
-        private async Task AssignSeatsIfDynamic(int vehicleId, List<CreateTravellerTicketDto> travelers)
+        private async Task AssignSeats(int vehicleId, List<CreateTravellerTicketDto> travelers)
         {
             var allSeats = await _seatRepository.GetSeatsByVehicleIdAsync(vehicleId);
-            var reservedSeats = allSeats.Where(x => x.Tickets.Any(y => y.TicketStatusId == (int) TicketStatusEnum.Reserved));
-
-            if (vehicleId == (int) VehicleTypeEnum.Bus)
+            var freeSeats = allSeats.Where(x => x.Tickets.All(y => y.TicketStatusId != (int)TicketStatusEnum.Reserved)).ToList();
+            int i = 0;
+            travelers.ForEach(x =>
             {
-                var seatIdsToReserve = travelers.Select(x => x.SeatId ?? 0).ToList();
-                if (seatIdsToReserve.Intersect(reservedSeats.Select(x => x.Id)).Any() || seatIdsToReserve.Any(x => x == 0))
-                {
-                    throw new Exception();
-                }
-            }
-            else
-            {
-                var freeSeats = allSeats.Where(x => x.Tickets.All(y => y.TicketStatusId != (int) TicketStatusEnum.Reserved)).ToList();
-                int i = 0;
-                travelers.ForEach(x =>
-                {
-                    x.SeatId = freeSeats[i].Id;
-                    i++;
-                });
-            }
-        }
-
-        private async Task UpsertTravellers(long id, List<CreateTravellerTicketDto> travelers)
-        {
-            foreach (var traveler in travelers)
-            {
-                var person = (await _personRepository.FindAsync(p => p.IdNumber == traveler.IdNumber && p.CreatorId == id))
-                    .FirstOrDefault();
-                if (person != null)
-                {
-                    traveler.Id = person.Id;
-                }
-
-                traveler.CreatorId = id;
-                var result = await _personService.UpsertPersonAsync(id, _mapper.Map<PersonDto>(traveler));
-                traveler.Id = result.Data;
-            }
+                x.SeatId = freeSeats[i].Id;
+                i++;
+            });
         }
     }
 }
